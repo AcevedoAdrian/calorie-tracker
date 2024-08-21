839f479ea3c14f2c92bf86f35781819189337496
AcevedoAdrian <acevedohugoadrian@gmail.com>
2024-08-21 13:06:18 -0300

    feat: Add form component for calorie tracking

--- src/componentes/Forms.tsx
+++ src/componentes/Forms.tsx
@@ -1,8 +1,37 @@
+import { useState, ChangeEvent, act, FormEvent } from 'react';
 import { categories } from '../db/categories';
+import { Activity } from '../types';
 
 export default function Form() {
+	const [activity, setActivity] = useState<Activity>({
+		category: 1,
+		nameActivity: '',
+		calories: 0,
+	});
+
+	const handleChange = (
+		event: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+	) => {
+		const isNumberFiel = ['category', 'calories'].includes(event.target.id);
+		setActivity({
+			...activity,
+			[event.target.id]: isNumberFiel
+				? Number(event.target.value)
+				: event.target.value,
+		});
+	};
+	const isValidActivity = () => {
+		const { nameActivity, calories } = activity;
+		return nameActivity.trim() !== '' && calories > 0;
+	};
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log(activity);
+	};
 	return (
-		<form className='space-y-5 bg-white shadow p-10 rounded-lg'>
+		<form
+			className='space-y-5 bg-white shadow p-10 rounded-lg'
+			onSubmit={handleSubmit}>
 			<div className='grid grid-cols-1 gap-3'>
 				<label
 					htmlFor='category'
@@ -11,7 +40,8 @@ export default function Form() {
 				</label>
 				<select
 					id='category'
-					className='border border-salate-300 p-2 rounded-lg w-full bg-white'>
+					className='border border-salate-300 p-2 rounded-lg w-full bg-white'
+					onChange={handleChange}>
 					{categories.map(category => (
 						<option
 							key={category.id}
@@ -23,15 +53,17 @@ export default function Form() {
 			</div>
 			<div className='grid grid-cols-1 gap-3'>
 				<label
-					htmlFor='activity'
+					htmlFor='nameActivity'
 					className='font-bold'>
 					Actividad:
 				</label>
 				<input
-					id='activity'
+					id='nameActivity'
 					type='text'
 					className='border border-salate-300 p-2 rounded-lg w-full bg-white'
 					placeholder='Ejemplo: Correr, Caminar, etc.'
+					value={activity.nameActivity}
+					onChange={handleChange}
 				/>
 			</div>
 			<div className='grid grid-cols-1 gap-3'>
@@ -45,11 +77,15 @@ export default function Form() {
 					type='number'
 					className='border border-salate-300 p-2 rounded-lg w-full bg-white'
 					placeholder='Ejemplo: 100 o 200'
+					value={activity.calories}
+					onChange={handleChange}
 				/>
 			</div>
 			<input
 				type='submit'
-				className='bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer'
+				className='bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10'
+				value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+				disabled={!isValidActivity()}
 			/>
 		</form>
 	);
---
+++ src/reducer/activityReducer.ts
@@ -0,0 +1,10 @@
+// Type Reducer, this is the file that will contain the reducer for the activity
+export type ActivityActions = {};
+
+type ActivityState = {};
+
+// Initial State, this is the default state of the reducer when the application starts
+export const initialState = {};
+
+// Reducer, this is the function that will change the state of the application
+export const activityReducer = () => {};
